ade5ce20ce4d6963150570d03655f56253a14fcd
trevor millner <trevormillner@yahoo.com>
2016-04-30 00:06:52 -0700

    task - Stuff db with real data

--- app/src/main/java/com/localhost/tmillner/similartemperature/db/WeatherContract.java
+++ app/src/main/java/com/localhost/tmillner/similartemperature/db/WeatherContract.java
@@ -14,26 +14,23 @@ public class WeatherContract {
     public static final String COLUMN_CITY = "city";
     public static final String COLUMN_COUNTRY_ID = "country_id";
     public static final String COLUMN_POPULATION = "population";
-    public static final String COLUMN_DATE_POPULATION_NOTED = "date_population_noted";
 
     public static final String CREATE_TABLE = "CREATE TABLE " + TABLE + "(" +
             COLUMN_ID + " integer primary key, " +
             COLUMN_CITY + " varchar(200), " +
             COLUMN_COUNTRY_ID + " integer, " +
             COLUMN_POPULATION + " long, " +
-            COLUMN_DATE_POPULATION_NOTED + " integer, " +
             "FOREIGN KEY(" + COLUMN_COUNTRY_ID + ") REFERENCES " +
             CountriesContract.TABLE + "(" + CountriesContract.COLUMN_ID + "))";
 
     public static final String ADD_SAMPLE_DATA = "INSERT INTO " + TABLE + "(" +
             COLUMN_CITY + ", " +
             COLUMN_COUNTRY_ID + ", " +
-            COLUMN_POPULATION + ", " +
-            COLUMN_DATE_POPULATION_NOTED + ")" + "VALUES" +
-            "('seattle','1',500000,2015)," +
-            "('austin','2',600000,2012)," +
-            "('chicago','1',700000,2010)," +
-            "('chinautla','3',75000,null)";
+            COLUMN_POPULATION + ")" + "VALUES" +
+            "('seattle','1',500000)," +
+            "('austin','2',600000)," +
+            "('chicago','1',700000)," +
+            "('chinautla','3',75000)";
 
     public static final String DROP_TABLE = "DROP TABLE " + TABLE +
             " CASCADE CONSTRAINTS";
--- app/src/main/java/com/localhost/tmillner/similartemperature/db/WeatherHelper.java
+++ app/src/main/java/com/localhost/tmillner/similartemperature/db/WeatherHelper.java
@@ -2,6 +2,7 @@ package com.localhost.tmillner.similartemperature.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -31,10 +32,9 @@ public class WeatherHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CountriesContract.CREATE_TABLE);
-        db.execSQL(WeatherContract.CREATE_TABLE);
-        // For previewing, populate w/some data
-        db.execSQL(WeatherContract.ADD_SAMPLE_DATA);
         populateCountriesData(db, CountriesContract.TABLE);
+        db.execSQL(WeatherContract.CREATE_TABLE);
+        populateCitiesData(db, WeatherContract.TABLE);
     }
 
     @Override
@@ -45,23 +45,7 @@ public class WeatherHelper extends SQLiteOpenHelper {
     }
 
     public void populateCountriesData(SQLiteDatabase db, String table) {
-        InputStream inputStream = null;
-        String fileOutput = "";
-        try {
-            int i;
-            inputStream = context.getResources().openRawResource(R.raw.countries);
-            while ((i=inputStream.read()) != -1) {
-                fileOutput += (char) i;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        String fileOutput = loadFile(R.raw.countries);
 
         try {
             JSONObject countries = new JSONObject(fileOutput);
@@ -82,6 +66,66 @@ public class WeatherHelper extends SQLiteOpenHelper {
     }
 
     public void populateCitiesData(SQLiteDatabase db, String table) {
+        String fileOutput = loadFile(R.raw.country_city_population);
 
+        try {
+            JSONObject records = new JSONObject(fileOutput);
+            JSONArray items = records.names();
+
+            for (int i = 0; i < items.length(); i++) {
+                String item = (String) items.get(i);
+                JSONObject itemJson = (JSONObject) records.get(item);
+                String country = itemJson.getString("country");
+                String city = itemJson.getString("city");
+                String population = itemJson.getString("population");
+                String countryCode = findCountryCode(db, country);
+
+                if (!countryCode.equals("")) {
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(WeatherContract.COLUMN_CITY, city);
+                    contentValues.put(WeatherContract.COLUMN_POPULATION, population);
+                    // Might need to be int...
+                    contentValues.put(WeatherContract.COLUMN_COUNTRY_ID, countryCode);
+                    db.insert(table, null, contentValues);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadFile(int resourceId) {
+        InputStream inputStream = null;
+        String fileOutput = "";
+        try {
+            int i;
+            inputStream = context.getResources().openRawResource(resourceId);
+            while ((i=inputStream.read()) != -1) {
+                fileOutput += (char) i;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return fileOutput;
+    }
+
+    // See helpers.PlacesResultDecoder
+    private String findCountryCode(SQLiteDatabase db, String country) {
+        String countryCode = "";
+        final String QUERY = "SELECT * FROM " + CountriesContract.TABLE + " +" +
+                "WHERE " + CountriesContract.COLUMN_COUNTRY_NAME  + " like %?%";
+        Cursor cursor = db.rawQuery(QUERY, new String[]{country});
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            countryCode = cursor.getString(cursor.
+                    getColumnIndex(CountriesContract.COLUMN_ID));
+        }
+        return countryCode;
     }
 }
